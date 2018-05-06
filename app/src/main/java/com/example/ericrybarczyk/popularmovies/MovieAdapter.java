911f95c436f77ae8acdf2acd0fb086ca61481e55
EricRybarczyk <EricRybarczyk@gmail.com>
2018-05-06 17:59:24 -0400

    Simple RecyclerView with TextView items is functional

--- app/src/main/java/com/example/ericrybarczyk/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/ericrybarczyk/popularmovies/MovieAdapter.java
@@ -1,15 +1,24 @@
 package com.example.ericrybarczyk.popularmovies;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder> {
 
+    private int numberOfMovies;
+    private List<String> movieList;
+
+    public MovieAdapter(MovieService movieService) {
+        this.movieList = movieService.getMovies();
+        this.numberOfMovies = this.movieList.size();
+    }
 
     @NonNull
     @Override
@@ -21,27 +30,30 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder>
 
     @Override
     public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
-
+        String movie = movieList.get(position);
+        holder.movieTextView.setText(movie);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return numberOfMovies;
     }
 
 
     public class MovieHolder extends RecyclerView.ViewHolder {
 
         ImageView movieImageView;
+        TextView movieTextView;
 
         public MovieHolder(View itemView) {
             super(itemView);
             // TODO - look into using Butterknife
-            movieImageView = itemView.findViewById(R.id.iv_movie_item);
+//            movieImageView = itemView.findViewById(R.id.iv_movie_item);
+            movieTextView = itemView.findViewById(R.id.iv_movie_item);
         }
 
         void bind() {
-            // TODO - I think this is where I will set up the image
+            // TODO - I think this is where I will set up the image ... or do I not really need this? Other demos just handle in constructor like above.
         }
     }
 
--- app/src/main/java/com/example/ericrybarczyk/popularmovies/MovieService.java
+++ app/src/main/java/com/example/ericrybarczyk/popularmovies/MovieService.java
@@ -1,7 +1,50 @@
 package com.example.ericrybarczyk.popularmovies;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieService {
-    //
+
+    private String movieApiKey;
 
 
+    public MovieService(Context context) {
+        Resources resources = context.getResources();
+        XmlResourceParser parser = resources.getXml(R.xml.keys);
+
+        // TODO - figure out how to do read the xml file
+
+        parser.close();
+    }
+
+    public List<String> getMovies() {
+
+        ArrayList<String> movies = new ArrayList<>();
+        movies.add("Star Wars");
+        movies.add("Star Trek");
+        movies.add("Space Balls");
+        movies.add("The Matrix");
+        movies.add("The Load Of The Rings");
+        movies.add("Iron Man");
+        movies.add("The Martian");
+        movies.add("The Hunt For Red October");
+        movies.add("Patriot Games");
+        movies.add("Guardians Of The Galaxy");
+        movies.add("Point Break");
+        movies.add("When Harry Met Sally");
+        movies.add("Goldeneye");
+        movies.add("Deadpool");
+        movies.add("Contact");
+        movies.add("The Terminator");
+        movies.add("Alien");
+        movies.add("The Abyss");
+        movies.add("Spider Man");
+        movies.add("The Wizard Of Oz");
+
+        return movies;
+    }
 }
